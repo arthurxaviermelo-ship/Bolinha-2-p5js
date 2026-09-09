300e48e82a3a1e7bf9f90eeefed10eba9ef22e98
arthurxaviermelo-ship <arthur.xavier.melo@escola.pr.gov.br>
2026-09-09 11:40:45 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,34 @@
+//Projeto Bolinha 02
+let yBolinha=200;
+let xBolinha=200;
+let raio = 15;
+let yVelocidade = 3;
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  mostraBolinha();
+   movimentaBolinha();
+  verificaColisao();  
+}
+
+//----------------Função mostra Bolinha--------------
+function mostraBolinha(){
+  circle(xBolinha,yBolinha,raio);
+}
+
+
+//-------------Função movimenta Bolinha------------
+function movimentaBolinha(){
+  yBolinha += yVelocidade 
+}
+
+
+//---------------Verifica colisão---------
+function verificaColisao(){
+  if(yBolinha > height|| yBolinha < 0){
+    yVelocidade *= -1;
+  }
+}
